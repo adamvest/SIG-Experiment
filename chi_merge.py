d6daca19169987a87c319c324ed6388aac67dc59
Adam Vest <apvest01@louisville.edu>
2017-10-24 19:49:58 -0400

    Broke chimerge + linear reg

--- chi_merge.py
+++ chi_merge.py
@@ -12,7 +12,42 @@ class ChiMergeOptions():
     def __init__(self):
         self.parser = argparse.ArgumentParser()
 
-        
-
     def parse_args(self):
         return self.parser.parse_args()
+
+
+def test_intervals(data, intervals, start):
+    k = len(set([x[1] for x in data]))
+    chi_value = 0
+
+    for i in range(2):
+        splice1 = data[intervals[start+i]:intervals[start+i+1]]
+        splice2 = data[intervals[start+i+1]:intervals[start+i+2]]
+
+        for j in range(k):
+            a_ij = len([x for x in splice1 if x[1] == j])
+            print "a_ij", a_ij
+            r_ij = intervals[start+i+1] - intervals[start+i]
+            print "r_ij", r_ij
+            c_j = len([x for x in splice1 if x[1] == j]) + len([x for x in splice2 if x[1] == j])
+            print "c_j", c_j
+            e_ij = max((r_ij * c_j) / (len(splice1) + len(splice2)), .1)
+            print "e_ij", e_ij
+            chi_value += (a_ij - e_ij) ** 2 / e_ij
+
+    return chi_value
+
+
+data = [(1.2, 1), (2.1, 1), (3.3, 0), (2.7, 0), (1.0, 1), (.8, 0), (2.2, 1), (4.3, 0), (.6, 0), (1.1, 1)]
+data.sort(key=lambda x: x[0])
+intervals = range(len(data) + 1)
+finished = False
+
+while finished == False:
+    chi_values = []
+
+    for i in range(len(data) - 1):
+        print test_intervals(data, intervals, i)
+        raw_input('stop')
+
+    print chi_values
---
+++ linear_regression.py
@@ -0,0 +1,66 @@
+'''
+ML/DL SIG experiment 4
+10/21
+Linear Regression using Gradient Descent
+'''
+
+
+import argparse
+import numpy as np
+from sklearn import datasets
+from pandas import DataFrame
+
+
+class LinearRegressionOptions():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+
+        self.parser.add_argument('--num_epochs', type=int, default=1000, help="number of epochs to train for")
+        self.parser.add_argument('--batch_size', type=int, default=16, help="number of samples per batch")
+        self.parser.add_argument('--lr', type=float, default=.001, help="gradient descent step size")
+        self.parser.add_argument('--train_test_split_ratio', type=float, default=.8, help="percentage of data to use for training")
+
+    def parse_args(self):
+        return self.parser.parse_args()
+
+
+#train a linear regression model
+def train_linear_regression_model(data_x, data_y, lr, num_epochs, batch_size):
+    weights = np.random.normal(0, .1, len(data_x[0]))
+    num_batches = len(data_x) / batch_size
+
+    for i in range(1, num_epochs+1):
+        print "\nBegin Training Epoch %d" % i
+
+        for j in range(num_batches):
+            batch_x = data_x[j * batch_size:(j+1) * batch_size]
+            batch_y = data_y[j * batch_size:(j+1) * batch_size]
+            batch_preds = np.dot(weights, batch_x.T)
+            diffs = batch_preds - batch_y
+            batch_loss = np.sum(.5 * np.square((diffs)))
+            grad = sum([diffs[i] * batch_x[i] for i in range(batch_size)])
+            weights -= lr * grad
+
+            print "Batch %d Loss: %g" % (j+1, batch_loss)
+
+    return weights
+
+
+if __name__ == "__main__":
+    args = LinearRegressionOptions().parse_args()
+
+    diabetes = datasets.load_diabetes()
+    data_x, data_y = diabetes.data, diabetes.target
+    data = np.column_stack((np.ones((len(data_x), 1)), data_x, data_y))
+    np.random.shuffle(data)
+    idx = int(args.train_test_split_ratio*len(data))
+    train, test = data[:idx], data[idx:]
+    train_x, train_y = train[:, :-1], train[:, -1]
+    test_x,  test_y = test[:, :-1], test[:, -1]
+
+    optimal_weights = train_linear_regression_model(train_x, train_y, args.lr, args.num_epochs, args.batch_size)
+
+    for i in range(len(test_x)):
+        print "\nPredicted Value:", np.dot(optimal_weights, test_x[i].T)
+        print "Actual Value:", test_y[i]
+        raw_input('stop')
