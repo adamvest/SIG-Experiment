e0dcdff000500bbdd01c64274ebd5618b1b4e644
Adam Vest <apvest01@louisville.edu>
2017-10-31 19:08:59 -0400

    Adding L2 regularization

--- linear_regression.py
+++ linear_regression.py
@@ -18,6 +18,7 @@ class LinearRegressionOptions():
         self.parser.add_argument('--num_epochs', type=int, default=1000, help="number of epochs to train for")
         self.parser.add_argument('--batch_size', type=int, default=16, help="number of samples per batch")
         self.parser.add_argument('--lr', type=float, default=.001, help="gradient descent step size")
+        self.parser.add_argument('--reg_strength', type=float, default=1e-3, help="L2 regularization strength")
         self.parser.add_argument('--train_test_split_ratio', type=float, default=.8, help="percentage of data to use for training")
 
     def parse_args(self):
@@ -25,7 +26,7 @@ class LinearRegressionOptions():
 
 
 #train a linear regression model
-def train_linear_regression_model(data_x, data_y, lr, num_epochs, batch_size):
+def train_linear_regression_model(data_x, data_y, lr, num_epochs, batch_size, reg_strength):
     weights = np.random.normal(0, .1, len(data_x[0]))
     num_batches = len(data_x) / batch_size
 
@@ -37,8 +38,8 @@ def train_linear_regression_model(data_x, data_y, lr, num_epochs, batch_size):
             batch_y = data_y[j * batch_size:(j+1) * batch_size]
             batch_preds = np.dot(weights, batch_x.T)
             diffs = batch_preds - batch_y
-            batch_loss = np.sum(.5 * np.square((diffs)))
-            grad = sum([diffs[i] * batch_x[i] for i in range(batch_size)])
+            batch_loss = np.sum(.5 * np.square((diffs))) + reg_strength * np.sum(np.square(weights))
+            grad = sum([diffs[i] * batch_x[i] for i in range(batch_size)]) + 2 * reg_strength * weights
             weights -= lr * grad
 
             print "Batch %d Loss: %g" % (j+1, batch_loss)
@@ -58,7 +59,7 @@ if __name__ == "__main__":
     train_x, train_y = train[:, :-1], train[:, -1]
     test_x,  test_y = test[:, :-1], test[:, -1]
 
-    optimal_weights = train_linear_regression_model(train_x, train_y, args.lr, args.num_epochs, args.batch_size)
+    optimal_weights = train_linear_regression_model(train_x, train_y, args.lr, args.num_epochs, args.batch_size, args.reg_strength)
 
     for i in range(len(test_x)):
         print "\nPredicted Value:", np.dot(optimal_weights, test_x[i].T)
